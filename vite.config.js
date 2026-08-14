541cbb3c6441a984b50a976bf18af0ab1d967166
injeell <hipolitoangel718@gmail.com>
2026-08-14 06:50:40 -0400

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/hipolito-module7-vue-system/',
   plugins: [
     vue(),
     tailwindcss()
   ]
-}) 
+})
